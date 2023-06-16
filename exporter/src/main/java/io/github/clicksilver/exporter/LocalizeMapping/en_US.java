c790dd476b6ce034d46ed21c81e64698196a26de
flier268 <flier268@gmail.com>
2023-06-17 01:37:32 +0800

    add: zh-TW and ja-JP support

---
+++ exporter/src/main/java/io/github/clicksilver/exporter/LocalizeMapping/en_US.java
@@ -0,0 +1,10 @@
+package io.github.clicksilver.exporter.LocalizeMapping;
+
+public class en_US implements ILocalizeMapping {
+
+    @Override
+    public String Map(String engName) {
+        return engName;
+    }
+
+}
